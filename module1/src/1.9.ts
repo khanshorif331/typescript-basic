305c1a09fb41bdc2f083141932fc6cc3970d66b1
khanshorif331 <khanshorif331@gmail.com>
2023-11-03 20:23:33 +0400

    add:type alias done with module no 1.9

--- module1/src/1.9.ts
+++ module1/src/1.9.ts
@@ -1,9 +1,44 @@
 {
 // type alias
 
+type Student = {
+    name : string,
+    age : number,
+    contactNo? : string,
+    gender : string,
+    address : string
+}
 
 
 
+const student1 :Student={
+    name: 'Shorif',
+    age: 25,
+    gender:'male',
+    contactNo: '01712345678',
+    address: 'Dhaka'
+}
+
+const student2 :Student={
+    name : 'Maamun',
+    age : 40,
+    gender : 'male',
+    address : 'Dubai'
+}
+
+
+// string 
+
+type UserName = string
+type IsAdmin = boolean
+
+const userName : UserName = 'Shorif'
+const isAdmin:IsAdmin = true
+
+// function
+type Add =(num1:number,num2:number)=>number
+
+const add:Add = (num1,num2)=> num1+num2
 
 
 
